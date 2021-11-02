962c6292b59233b7cfef6aac159923b9c15ee76e
oferst13 <oferst13@gmail.com>
2021-11-02 22:00:10 +0200

    improved plot

--- plot_release_policy.py
+++ plot_release_policy.py
@@ -7,12 +7,13 @@ plt.rc('font', size=11)
 plot_hours = np.ceil(bm.zero_Q * source.dt / 3600)
 t = np.arange(np.shape(source.release)[1] + 1)
 releases_2plot = np.c_[source.release, np.zeros(3)]
-ls=['-','-.',':']
-cl=['xkcd:dark sky blue','r','xkcd:goldenrod']
+ls=['-', '-.', ':']
+cl=['xkcd:dark sky blue', 'r', 'xkcd:goldenrod']
 for gg, graph in enumerate(releases_2plot):
     plt.step(t, graph*10, cl[gg], where='post', label=f'Tank {gg+1}', linewidth=4-0.7*gg, linestyle=ls[gg])
-fig=plt.gcf()
-fig.set_size_inches(6,4)
+fig = plt.gcf()
+plt.xticks(np.arange(0, np.shape(source.release)[1] + 1, 1.0))
+fig.set_size_inches(6, 4)
 fig.tight_layout(pad=1.5)
 plt.legend(loc='lower center', bbox_to_anchor=(0.6,0))
 plt.xlabel('t (hours)')
